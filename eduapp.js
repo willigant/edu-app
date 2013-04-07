c83c588188a1e8c5c3c1fce982d0858bb776d8bd
Khaliq Gant <khaliqgant@gmail.com>
2013-04-07 13:47:26 -0400

    removing needless eduapp.js file

--- eduapp.js
+++
@@ -1,9 +0,0 @@
-//declare collections
-Students = new Meteor.Collection("students");
-Questions = new Meteor.Collection("questions");
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
