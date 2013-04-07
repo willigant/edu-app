1af9c2ce5cdfbcd571390aca600043fea8cd2e5c
Khaliq Gant <khaliqgant@gmail.com>
2013-04-07 13:41:40 -0400

    fixing dir

---
+++ client/client.js
@@ -0,0 +1,12 @@
+  Template.hello.greeting = function () {
+    return "This is a simple version of an Education App For Travis";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+
