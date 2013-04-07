c21c2a27de914bfd3dfc46d6890b70798cad5ae1
Khaliq Gant <khaliqgant@gmail.com>
2013-04-07 12:56:06 -0400

    initial commit with files

---
+++ eduapp.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "This is a simple version of an Education App For Travis";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
